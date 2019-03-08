bbe2d7bd5f016fdad99c50ce77652bcfca907497
hugo <hugohuang1111@gmail.com>
2019-03-08 10:37:30 +0800

    add help

--- bcx/main.js
+++ bcx/main.js
@@ -2,6 +2,7 @@
 
 const fse = require('fs-extra');
 const Path = require("path");
+const { shell } = require('electron')
 
 module.exports = {
     messages: {
@@ -10,6 +11,9 @@ module.exports = {
             const destFolder = Path.join(Editor.projectInfo.path, 'assets');
             fse.copySync(srcFolder, destFolder);
             Editor.log('BCX plugin is installed to assets');
+        },
+        "help": function() {
+            shell.openExternal('https://github.com/sdkbox/bcx-sdk-creator/blob/master/readme.md');
         }
     },
 };
