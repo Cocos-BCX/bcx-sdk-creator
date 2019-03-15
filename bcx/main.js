04a3223b5ebb068bac89fa008c35e73ab6e64d32
hugo <hugohuang1111@gmail.com>
2019-03-15 18:57:57 +0800

    fix project path

--- bcx/main.js
+++ bcx/main.js
@@ -7,8 +7,14 @@ const { shell } = require('electron')
 module.exports = {
     messages: {
         'install': function() {
+            const getProjectPath = function() {
+                if (Editor.Project && Editor.Project.path) {
+                    return Editor.Project.path;
+                }
+                return Editor.projectInfo.path
+            }
             const srcFolder = Path.join(__dirname, 'plugin');
-            const destFolder = Path.join(Editor.projectInfo.path, 'assets');
+            const destFolder = Path.join(getProjectPath(), 'assets');
             fse.copySync(srcFolder, destFolder);
             Editor.log('BCX plugin is installed to assets');
         },
